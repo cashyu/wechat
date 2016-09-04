40e93d2129a62d1a5b28a83667d27f5e12e01aec
yuxinhua0 <1594466572@qq.com>
2016-09-04 22:44:44 +0800

    生成票据access_token

--- app.js
+++ app.js
@@ -4,14 +4,23 @@
 'use strict'
 
 var koa = require('koa');
+var path = require('path');
 var wechat = require('./wechat/g');
-var sha1 = require('sha1');
+var util = require('./libs/util');
+var wechat_file = path.join(__dirname,'./config/wechat.txt');
 
 var config = {
     wechat: {
         appID: 'wxb80e5bddb2d804f3',
         appSecret: '5c556435736411c7c5c155da6018adb2',
-        token: 'imoocwechatyuxinhuachat'
+        token: 'imoocwechatyuxinhuachat',
+        getAccessToken:function(){
+            return util.readFileAsync(wechat_file);
+        },
+        saveAccessToken:function(data){
+            data = JSON.stringify(data);    //转成字符串
+            return util.writeFileAsync(wechat_file,data);
+        }
     }
 };
 var app = new koa();
--- wechat/g.js
+++ wechat/g.js
@@ -4,16 +4,89 @@
 'use strict'
 
 var sha1 = require('sha1');
+var Promise = require('bluebird');
+var request = Promise.promisify(require('request'));    //将request进行promise化
 
-var config = {
-    wechat: {
-        appID: 'wxb80e5bddb2d804f3',
-        appSecret: '5c556435736411c7c5c155da6018adb2',
-        token: 'imoocwechatyuxinhuachat'
+
+var prefix = 'https://api.weixin.qq.com/cgi-bin/';
+var api = {
+    accessToken:prefix +
+    'token?grant_type=client_credential'
+};
+
+function Wechat(opts){
+    var that = this;
+    this.appID = opts.appID;
+    this.appSecret = opts.appSecret;
+    this.getAccessToken = opts.getAccessToken;
+    this.saveAccessToken = opts.saveAccessToken;
+
+    this.getAccessToken()
+        .then(function(data){
+            try {
+                data = JSON.parse(data);
+            }
+            catch(e){
+                return that.updateAccessToken(data);
+            }
+
+            if(that.isValidAccessToken(data)){
+                Promise.resolve(data)
+            }else{
+                return that.updateAccessToken();
+            }
+        })
+        .then(function(data){
+
+            that.access_token = data.access_token;
+            that.expires_in = data.expires_in;
+
+            that.saveAccessToken(data);
+        })
+}
+
+Wechat.prototype.isValidAccessToken = function(data){
+    if(!data || !data.access_token || !data.expires_in){
+        return false;
+    }else{
+        var access_token = data.access_token;
+        var expires_in = data.expires_in;
+        var now = (new Date().getTime());
+
+        if(now < expires_in){
+            return true;
+        }else{
+            return false;
+        }
     }
 };
 
+Wechat.prototype.updateAccessToken = function(){
+    var appID = this.appID;
+    var appSecret = this.appSecret;
+    var url = api.accessToken + '&appid=' + appID + '&secret=' + appSecret;
+
+    return new Promise(function(resolve,reject){
+        request({url:url,json:true}) //向服务器发起请求(GET,POST.....)
+            .then(function(response){
+                var data = response.body;
+                console.log("+++++++++++");
+                console.log(data);
+                var now = (new Date().getTime());
+                var expires_in = now + (data.expires_in - 20)*1000; //考虑延时，提前20s刷新token
+
+                data.expires_in = expires_in;
+
+                resolve(data);
+            })
+    })
+}
+
+
+
 module.exports = function(opts){
+    var wechat = new Wechat(opts); //new出的Wechat，管理票据和微信接口
+
     return function *(next){
         console.log(this.query);
         var token = opts.token;
